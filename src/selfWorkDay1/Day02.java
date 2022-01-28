caa8c7730d4cbbfa67f6e268400d718c08138fe2
Omer Guven <omerguvennnn@hotmail.com>
2022-01-28 16:17:57 -0500

    day2

---
+++ src/selfWorkDay1/Day02.java
@@ -0,0 +1,11 @@
+package selfWorkDay1;
+
+public class Day02 {
+    public static void main(String[] args) {
+        System.out.println("today weather is still same but day goes really fast ");
+
+
+
+
+    }
+}
