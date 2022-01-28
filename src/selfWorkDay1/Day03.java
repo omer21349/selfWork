fb054dfaeeef85f3237295585599f1758c2b8e83
Omer Guven <omerguvennnn@hotmail.com>
2022-01-28 18:55:06 -0500

    day4

--- src/selfWorkDay1/Day03.java
+++ src/selfWorkDay1/Day03.java
@@ -3,5 +3,7 @@ package selfWorkDay1;
 public class Day03 {
     public static void main(String[] args) {
         System.out.println("school didn't finish yet");
+
+        // today is so hot
     }
 }
