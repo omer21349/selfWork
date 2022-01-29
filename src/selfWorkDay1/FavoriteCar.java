4a574c9253851b7f9c42ee775671404ad7a070c0
Omer Guven <omerguvennnn@hotmail.com>
2022-01-28 19:40:33 -0500

    need money

--- src/selfWorkDay1/FavoriteCar.java
+++ src/selfWorkDay1/FavoriteCar.java
@@ -3,6 +3,7 @@ package selfWorkDay1;
 public class FavoriteCar {
     public static void main(String[] args) {
         System.out.println("i want to by new car for myself");
+        System.out.println("but i need a money for buying good car");
 
     }
 }
