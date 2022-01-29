b137072f7ffcbeb3f91bd733cd9bf6f2fb09e73a
Omer Guven <omerguvennnn@hotmail.com>
2022-01-28 19:34:07 -0500

    so many option

--- src/selfWorkDay1/Holiday.java
+++ src/selfWorkDay1/Holiday.java
@@ -3,5 +3,6 @@ package selfWorkDay1;
 public class Holiday {
     public static void main(String[] args) {
         System.out.println("looking place for holiday");
+        //there is so many option out there
     }
 }
