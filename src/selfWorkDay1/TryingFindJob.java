19ee64a44d914c149c32beeb9b8d9cefdb10266e
Omer Guven <omerguvennnn@hotmail.com>
2022-01-28 19:26:48 -0500

    thinking

--- src/selfWorkDay1/TryingFindJob.java
+++ src/selfWorkDay1/TryingFindJob.java
@@ -4,6 +4,7 @@ public class TryingFindJob {
     public static void main(String[] args) {
         System.out.println("today it was not much job");
 
+        // i thing in couple mount i will find some job
 
 
 
