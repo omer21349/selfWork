9be05f736cbf1a62c2e747ccbde739522305c5b8
Omer Guven <omerguvennnn@hotmail.com>
2022-01-28 19:35:03 -0500

    found place

--- src/selfWorkDay1/Holiday.java
+++ src/selfWorkDay1/Holiday.java
@@ -4,5 +4,6 @@ public class Holiday {
     public static void main(String[] args) {
         System.out.println("looking place for holiday");
         //there is so many option out there
+        //but i will choose colorado
     }
 }
